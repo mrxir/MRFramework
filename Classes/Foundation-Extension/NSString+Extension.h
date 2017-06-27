749bc29eb57af8569dadd415f3de713458505544
我也没有时光机器 <100885521@qq.com>
2017-06-27 16:07:45 +0800

    add NSString cipher and drawing extension
    包括 md5 字符串, md5 文件, 字符串所占区域计算

--- Classes/Foundation-Extension/NSString+Extension.h
+++ Classes/Foundation-Extension/NSString+Extension.h
@@ -29,3 +29,63 @@
 + (BOOL)isValidString:(id)obj;
 
 @end
+
+
+
+#pragma mark - cipher extension
+
+#import <CommonCrypto/CommonDigest.h>
+
+/**
+ cipher extension
+ */
+@interface NSString (Cipher)
+
+#pragma mark - cipher - MD5
+
+/**
+ 返回该字符串的MD5Hash值
+ 
+ @return NSString
+ */
+- (NSString *)md5Hash;
+
+/**
+ 返回该文件的MD5Hash值
+ 
+ @param filePath 文件路径
+ @return NSString
+ */
++ (NSString *)md5HashWithFile:(NSString *)filePath;
+
+@end
+
+
+
+#pragma mark - drawing extension
+
+#import <UIKit/NSStringDrawing.h>
+#import <UIKit/NSParagraphStyle.h>
+#import <UIKit/UIFont.h>
+
+/**
+ drawing extension
+ */
+@interface NSString (Drawing)
+
+typedef NS_ENUM(NSUInteger, CalculateOption) {
+    CalculateOptionWidth,
+    CalculateOptionHeight,
+};
+
+/**
+ 返回字符串的边界
+ 
+ @param font 当前字符串使用的字体
+ @param frame 当前字符串的显示范围
+ @param option 计算宽度或者高度选项
+ @return CGRect
+ */
+- (CGRect)boundingRectWithFont:(UIFont *)font frame:(CGRect)frame CalculateOption:(CalculateOption)option;
+
+@end
